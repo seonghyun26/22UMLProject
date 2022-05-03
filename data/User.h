1002ce58637010f9abec837d4e957cd532c38593
mcastillon <mario.castillon@insa-lyon.fr>
2022-05-03 14:59:26 +0200

    structure

---
+++ data/User.h
@@ -0,0 +1,26 @@
+#ifndef USER_H
+#define USER_H
+
+class User{
+procted:
+	long id;
+	string login;
+	string password;
+	int type;
+
+public:
+	long getId(){return id;}
+	string getLogin(){return login;}
+	string getPassword(){return password;}
+	int getType(){return type;}
+	
+
+	
+set(	long id){this.id=};
+	string login;
+	string password;
+	int type;
+
+
+}
+#endif
