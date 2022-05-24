f48cb4ce74910e13b2a1746b5faa634155fa8c83
hyun <hyun11262000@gmail.com>
2022-05-24 16:37:39 +0200

    Class Cleaner - fixed tm*

--- data/Cleaner.h
+++ data/Cleaner.h
@@ -13,26 +13,26 @@ class Cleaner
 protected:
   Provider *provider;
   string id;
-  struct tm *startTM;
-  struct tm *endTM;
+  struct tm startTM;
+  struct tm endTM;
   double latitude;
   double longitude;
 
 public:
-  Cleaner(Provider *provider_, string id_, struct tm *start_tm_, struct tm *end_tm_, double latitude_, double longitude_);
+  Cleaner(Provider *provider_, string id_, struct tm start_tm_, struct tm end_tm_, double latitude_, double longitude_);
 
   void setProvider(Provider *provider);
   void setId(string id);
-  void setStartTM(struct tm *start_tm);
-  void setEndTM(struct tm *end_tm);
+  void setStartTM(struct tm start_tm);
+  void setEndTM(struct tm end_tm);
   void setLatitude(double latitude);
   void setLongitude(double longitude);
 
   Provider *getProvider();
   string getId();
-  struct tm *getStartTM();
-  struct tm *getEndTM();
-  double getLatitude(); 
+  struct tm getStartTM();
+  struct tm getEndTM();
+  double getLatitude();
   double getLongitude();
 };
 
