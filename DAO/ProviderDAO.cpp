6b7b2dc2ad6cd52c499510b5237c7bfd1c900aa6
mcastillon <mario.castillon@insa-lyon.fr>
2022-05-30 21:52:32 +0200

    Revert "providerDAO"
    This reverts commit 198c9d740ecd8ca400646ef03398ef67256ed186.

--- DAO/ProviderDAO.cpp
+++ DAO/ProviderDAO.cpp
@@ -58,7 +58,6 @@ void ProviderDAO::parseProviderAndCleaner(string providersPath,string cleanersPa
 		fs.getline(forgot,256);
 	}
 
-	fs.close();
 	fs.open (providersPath, fstream::in);
 	while(fs.getline(providerId,32,';') || strlen(providerId)!=0){
 		fs.getline(cleanerId,32,';');
@@ -69,9 +68,7 @@ void ProviderDAO::parseProviderAndCleaner(string providersPath,string cleanersPa
 		}
 		currentProvider= &providerList[providerById[providerId]];
 		currentCleaner= &cleanerList[cleanerById[cleanerId]];
-		currentCleaner->setProvider(currentProvider);
 		currentProvider->addCleaner(currentCleaner);
 		fs.getline(forgot,256);
 	}
-	fs.close();
 }
